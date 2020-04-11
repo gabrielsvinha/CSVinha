932428733cfa6411e036d9d92717d048610eeef9
gabrielsvinha <gabriel.vinha@vtex.com.br>
2020-04-11 17:44:08 -0300

    Initial commit

---
+++ Core/CSVParser.fs
@@ -0,0 +1,15 @@
+﻿namespace CSVinha.Core.CSVParser
+
+open System.IO
+open System.Collections
+
+module Parser =
+    let SerializeStream (stream: StreamReader) =
+        printfn "Parse func"
+
+    let Serialize (filePath: string) =
+        use sr = new StreamReader (filePath)
+        sr |> SerializeStream
+
+    let TryDeserialize (csv: IDictionary) =
+        printfn "deserialize"
---
+++ README.md
@@ -0,0 +1,5 @@
+# CSVinha
+
+This is the simplest way to serialize/deserialize CSV files in F#.
+
+This is also just a study project.
